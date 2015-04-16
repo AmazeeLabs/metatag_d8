cec682a1bd497cf5cffe27f6fd7e2a8a1f36f81e
Damien McKenna <damien@mc-kenna.com>
2015-04-15 20:31:27 -0400

    Added a group for Facebook tags.

---
+++ src/Plugin/metatag/Group/Facebook.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\metatag\Plugin\metatag\Group\Facebook.
+ */
+
+namespace Drupal\metatag\Plugin\metatag\Group;
+
+use Drupal\metatag\Plugin\metatag\Group\GroupBase;
+
+/**
+ * The basic group.
+ *
+ * @MetatagGroup(
+ *   id = "facebook",
+ *   label = @Translation("Facebook"),
+ *   description = @Translation("Facebook"),
+ *   weight = 6
+ * )
+ */
+class Facebook extends GroupBase {
+  // Inherits everything from Base.
+}
