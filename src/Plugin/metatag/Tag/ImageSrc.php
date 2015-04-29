9b1bb600866dac0e013b4563159d5df85a75a45d
Damien McKenna <damien@mc-kenna.com>
2015-04-29 04:21:12 -0400

    ImageSrc tag moved into 'advanced' group.

--- src/Plugin/metatag/Tag/ImageSrc.php
+++ src/Plugin/metatag/Tag/ImageSrc.php
@@ -11,14 +11,14 @@ use Drupal\metatag\Plugin\metatag\Tag\MetaNameBase;
 use Drupal\metatag\Annotation\MetatagTag;
 
 /**
- * The basic "Keywords" meta tag.
+ * The advanced "Image" meta tag.
  *
  * @MetatagTag(
  *   id = "image_src",
  *   label = @Translation("Image"),
  *   description = @Translation("An image associated with this page, for use as a thumbnail in social networks and other services."),
  *   name = "image_src",
- *   group = "basic",
+ *   group = "advanced",
  *   weight = 4
  * )
  */
